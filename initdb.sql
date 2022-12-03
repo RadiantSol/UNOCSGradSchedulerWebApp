e2920e3fdb28ba6988d8600cb250ef157bd1edfb
Jober't Aladwan <Jobert.Aladwan@gmail.com>
2022-12-03 12:25:53 -0600

    Updated files to attempt to work with AWS ELB

--- initdb.sql
+++ initdb.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE IF NOT EXISTS mydb;
+CREATE DATABASE IF NOT EXISTS webappdb;
 DROP TABLE IF EXISTS classes, users;
 CREATE TABLE classes (id INT, classname VARCHAR(255), semester ENUM('Spring', 'Fall'), schoolyear YEAR, startTime TIME, credithours INT, PRIMARY KEY (id));
-INSERT INTO classes VALUES (5452, 'Cloud Computing', 'Fall', 2022, '5:30', 3);
+INSERT INTO classes VALUES (5452, 'Cloud Computing', 'Fall', 2022, '17:00', 3);
+INSERT INTO classes VALUES (6454, 'Parallel & Scientific Computing', 'Fall', 2022, '15:30', 3);
+INSERT INTO classes VALUES (6645, 'Planning Algorithms in AI', 'Fall', 2022, '9:30', 3);
