6ef57197cdb29a71546285465b9b8d91747d2eae
Jober't Aladwan <Jobert.Aladwan@gmail.com>
2022-12-03 01:18:21 -0600

    added EJS functionality and converted html files to EJS for dynamic HTML formatting. Started Scheduler page for retrieval of class list to user.

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const fs = require("fs");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+// dynamically change HTML pages using ejs
+// https://www.geeksforgeeks.org/how-to-dynamically-add-html-content-when-documents-are-inserted-in-collection-using-node-js/
+app.set('view engine', 'ejs');
 const con = mysql.createConnection({
     host: "localhost",
     user: "MasterUser",
@@ -19,19 +23,28 @@ con.connect(function (err) {
     con.query(sql, function (err, result) {
         if(err) throw err;
         console.log("Result: " + result);
-    })
+    });
 });
 
 // default page for user
 app.get("/", async (req, res) => {
-    res.sendFile(__dirname + "/pages/home.html");
+    res.render("home");
 });
 
-app.get("/test", async (req, res) => {
-    res.send("Youre not supposed to see this!");
-})
-
-// helper function to initialize database and populate tables
+app.get("/scheduler", async (req, res) => {
+    con.connect(function (err) {
+        if(err) throw err;
+        console.log("Connected to class db");
+        con.query("SELECT * FROM classes", function (err, result) {
+            if(err) throw err;
+            classList = result;
+            console.log("Result: " + result);
+            // if result list is null, replace with "problem fetching classes!"
+            result = result ? result : "problem fetching classes from db!";
+            res.render("scheduler", {classes: result});
+        });
+    });
+});
 
 const server = app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}/`);
