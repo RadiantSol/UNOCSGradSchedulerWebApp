ed8447c419a66901042faae7d0029ff08234e510
Jober't Aladwan <radiant@Joberts-MacBook-Air.local>
2022-12-08 17:05:26 -0600

    added beginnings of code (commented out) for AWS polly transcription

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
+// aws polly code
+// const { StartSpeechSynthesisTaskCommand } = require("@aws-sdk/client-polly");
+// const { pollyClient } = require("./libs/pollyClient.js");
+
+// // Create the parameters
+// var params = {
+//   OutputFormat: "mp3",
+//   OutputS3BucketName: "videoanalyzerbucket",
+//   Text: "Hello David, How are you?",
+//   TextType: "text",
+//   VoiceId: "Joanna",
+//   SampleRate: "22050",
+// };
+
+// const run = async () => {
+//   try {
+//     await pollyClient.send(new StartSpeechSynthesisTaskCommand(params));
+//     console.log("Success, audio file added to " + params.OutputS3BucketName);
+//   } catch (err) {
+//     console.log("Error putting object", err);
+//   }
+// };
+// run();
+
+
 
 const PORT = process.env.PORT || 8080;
 const app = express();
